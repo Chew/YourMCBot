e3ffe5e7a33b330bc23281355f3def09dc744323
Chewsterchew <chew@chew.pw>
2018-03-14 08:52:05 -0500

    Use fields to show partnership goals

--- bot.rb
+++ bot.rb
@@ -25,7 +25,9 @@ bot.command(:partnership) do |event|
   bot.channel(event.channel.id.to_s).send_embed do |e|
     e.title = 'YourMCAdmin\'s Road to Partnership'
 
-    e.description = ['Youtube Stats', "Subs: #{subs} / 100,000 (#{100_000 - subs} to go!)", '', 'Mixer Stats:', "Followers: #{follows} / 10,000 (#{10_000 - follows} to go!)"].join("\n")
+    e.add_field(name: 'YouTube Stats', value: "Subs: #{subs} / 100,000 (#{100_000 - subs} to go!)", inline: false)
+    e.add_field(name: 'Mixer Stats', value: "Followers: #{follows} / 10,000 (#{10_000 - follows} to go!)", inline: false)
+
     e.color = '00FF00'
   end
 end
