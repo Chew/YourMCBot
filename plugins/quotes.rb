61d2b6cf7c8a5ec730891bdfe1ef14fcbf619aa9
Chewsterchew <chew@chew.pw>
2018-06-11 08:52:55 -0500

    Quotes are now 95% stable!

--- plugins/quotes.rb
+++ plugins/quotes.rb
@@ -1,11 +1,17 @@
 module Quotes
   extend Discordrb::Commands::CommandContainer
 
-  command(:quote, min_args: 0, max_args: 1) do |event, num = -1|
+  command(:quote, min_args: 0, max_args: 1) do |event, num = 'none specified'|
     quotes = JSON.parse(RestClient.get("#{BASEURL}/showquotes",
                                        params: { chanid: CHANID },
                                        :'Content-Type' => :json))
 
+    num = rand(0..num.length - 1).to_i if num == 'none specified'
+    num = num.to_i
+    if num >= quotes.length || num.negative?
+      event.respond "No quote with that ID! Try again buddy. There are #{quotes.length} quotes! (Starts with 0)"
+      return
+    end
     quote = quotes[num.to_i]['quote']
     spots = quote.split(' - ')
     qu = spots[0]
