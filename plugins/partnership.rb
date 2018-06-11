b0df820eb87ad019faf224b3e219d9ef1adf1305
Chewsterchew <chew@chew.pw>
2018-06-10 20:21:17 -0500

    Prefer event.channel and fix module name

--- plugins/partnership.rb
+++ plugins/partnership.rb
@@ -1,10 +1,10 @@
-module Mixer
+module Partnership
   extend Discordrb::Commands::CommandContainer
 
   command(:partnership) do |event|
     subs = JSON.parse(RestClient.get("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername=yourmcadmin&key=#{CONFIG['google']}"))['items'][0]['statistics']['subscriberCount'].to_i
     follows = JSON.parse(RestClient.get('https://mixer.com/api/v1/channels/yourmcadmin'))['numFollowers'].to_i
-    bot.channel(event.channel.id.to_s).send_embed do |e|
+    event.channel.send_embed do |e|
       e.title = 'YourMCAdmin\'s Road to Partnership'
 
       e.add_field(name: 'YouTube Stats', value: "Subs: #{subs} / 100,000 (#{100_000 - subs} to go!)", inline: false)
