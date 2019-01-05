4584da2a19cfdac83febac4b865554a5a53901c9
Chewsterchew <chew@chew.pw>
2019-01-05 10:39:34 -0600

    Add reload command

--- bot.rb
+++ bot.rb
@@ -17,15 +17,27 @@ Bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'], client_id: CON
 
 puts 'Initial Startup complete, loading all commands...'
 
-Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each { |file| require file }
+def loadpls
+  Bot.clear!
+  Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each do |wow|
+    load wow
+    require wow
+    bob = File.readlines(wow) { |line| line.split.map(&:to_s).join }
+    command = bob[0][7..bob[0].length]
+    command.delete!("\n")
+    command = Object.const_get(command)
+    Bot.include! command
+    puts "Plugin #{command} successfully loaded!"
+  end
+end
 
-Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each do |wow|
-  bob = File.readlines(wow) { |line| line.split.map(&:to_s).join }
-  command = bob[0][7..bob[0].length]
-  command.delete!("\n")
-  command = Object.const_get(command)
-  Bot.include! command
-  puts "Plugin #{command} successfully loaded!"
+loadpls
+
+Bot.command(:reload) do |event|
+  break unless event.user.id == CONFIG['owner_id']
+
+  loadpls
+  event.respond 'Reloaded sucessfully!'
 end
 
 puts 'Done loading plugins! Finalizing start-up'
