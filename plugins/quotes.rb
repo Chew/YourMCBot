1f4438911af32639cebad71dba6e65c7bcf65411
Chewsterchew <chew@chew.pw>
2019-01-09 17:33:28 -0600

    General fixes

--- plugins/quotes.rb
+++ plugins/quotes.rb
@@ -4,12 +4,12 @@ module Quotes
   command(:quote, min_args: 0, max_args: 1) do |event, num = 'none specified'|
     quotes = JSON.parse(RestClient.get("#{BASEURL}/showquotes",
                                        params: { chanid: CHANID },
-                                       :'Content-Type' => :json))
+                                       'Content-Type': :json))
 
     num = rand(0..num.length - 1).to_i if num == 'none specified'
     num = num.to_i
     if num >= quotes.length || num.negative?
-      event.respond "No quote with that ID! Try again buddy. There are #{quotes.length} quotes! (Starts with 0)"
+      event.respond "No quote with that ID! Try again buddy. There are #{quotes.length} quotes! (Starts at 0)"
       return
     end
     quote = quotes[num.to_i]['quote']
