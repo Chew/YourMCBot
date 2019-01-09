b8f089e18de96e7413f2faf5b706ab1f4c485546
Chewsterchew <chew@chew.pw>
2019-01-09 17:33:59 -0600

    rubocop fixes and code bettering

--- plugins/help.rb
+++ plugins/help.rb
@@ -30,32 +30,36 @@ module Help
     end
   end
 
-  command(%i[bug feedback suggestion error report], min_args: 1) do |event, *feedback|
+  command(:bug, aliases: %i[feedback suggestion error report], min_args: 1) do |event, *feedback|
     event.message.delete
     event.send_temporary_message(event.user.mention + ', Your feedback has been successfully recorded. Your message and username have been sent.', 5)
 
     feedback = feedback.join(' ')
 
-    Bot.channel(423127606880370688).send_embed("<@#{CONFIG['owner_id']}>") do |embed|
+    Bot.channel(423_127_606_880_370_688).send_embed("<@#{CONFIG['owner_id']}>") do |embed|
       embed.title = 'New Feedback!'
       embed.colour = '6166A8'
       embed.description = feedback.to_s
       embed.timestamp = Time.now
 
-      begin
-        RestClient.get("https://cdn.discordapp.com/avatars/#{event.user.id}/#{event.user.avatar_id}.gif?size=1024")
-        embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: event.user.distinct.to_s, icon_url: "https://cdn.discordapp.com/avatars/#{event.user.id}/#{event.user.avatar_id}.gif?size=1024")
-      rescue RestClient::UnsupportedMediaType
-        embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: event.user.distinct.to_s, icon_url: "https://cdn.discordapp.com/avatars/#{event.user.id}/#{event.user.avatar_id}.webp?size=1024")
-      end
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: event.user.distinct.to_s, icon_url: event.user.avatar_url)
 
       embed.add_field(name: 'User ID', value: event.user.id.to_s, inline: true)
       if event.channel.pm?
         embed.add_field(name: 'Server', value: 'Sent from a PM', inline: true)
       else
-        embed.add_field(name: 'Server', value: "Name: #{event.server.name}\n#{event.server.id}", inline: true)
+        embed.add_field(name: 'Server', value: "Name: #{event.server.name}\nID: #{event.server.id}", inline: true)
       end
     end
     # Bot.user(CONFIG['owner_id']).pm(args.join(' '))
+  end
+
+  command(:ping, min_args: 0, max_args: 1) do |event, noedit|
+    if noedit == 'noedit'
+      event.respond "Pong! Time taken: #{((Time.now - event.timestamp) * 1000).to_i} milliseconds."
+    else
+      m = event.respond('Pinging...')
+      m.edit "Pong!! Time taken: #{((Time.now - event.timestamp) * 1000).to_i} milliseconds."
+    end
   end
 end
