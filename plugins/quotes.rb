eea8818abfa6d182f4b295cfba0bb8c04eab5d41
Chewsterchew <chew@chew.pw>
2019-01-10 16:13:42 -0600

    show quote number and correct date

--- plugins/quotes.rb
+++ plugins/quotes.rb
@@ -6,7 +6,7 @@ module Quotes
                                        params: { chanid: CHANID },
                                        'Content-Type': :json))
 
-    num = rand(0..num.length - 1).to_i if num == 'none specified'
+    num = rand(0..quotes.length - 1).to_i if num == 'none specified'
     num = num.to_i
     if num >= quotes.length || num.negative?
       event.respond "No quote with that ID! Try again buddy. There are #{quotes.length} quotes! (Starts at 0)"
@@ -17,15 +17,21 @@ module Quotes
     qu = spots[0]
     author = spots[1].delete('@')
     date = spots[2].split(' ')[0]
+
+    dataes = date.split('/')
+    month = dataes[0]
+    day = dataes[1]
+    year = dataes[2]
+
     game = spots[2].split(' ')[1].delete('(').delete(')')
 
     event.channel.send_embed do |embed|
       embed.colour = 0xd084
       embed.description = qu.to_s
 
-      embed.timestamp = Time.new(date)
+      embed.timestamp = Time.new(year, month, day)
 
-      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "Quote from #{author}", url: "http://mixer.com/#{author}")
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "Quote \##{num} from #{author}", url: "http://mixer.com/#{author}")
       embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Game: #{game}")
     end
   end
